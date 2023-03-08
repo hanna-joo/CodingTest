514436a62b5d61aa32f6940837d87df4097e9b23
hanna-joo <wnehfehf@gmail.com>
2023-03-08 23:32:05 +0900

    Update second solution using sliding window

--- python/baekjoon/doit_09.py
+++ python/baekjoon/doit_09.py
@@ -4,22 +4,75 @@
 # 임의의 DNA 문자열과 길이, 비밀번호 길이, 최소 조건이 주어졌을 때 만들 수 있는 비밀 번호의 종류의 수를 구하라.
 # 문자열 길이 |S|와 비밀번호 길이 |P|(1<=|P|<=|S|<=1,000,000)
 
-# 필요 변수 선언 및 초기화
+# 슬라이딩 윈도우 활용 X
+'''
 dna_len, pwd_len = map(int, input().split())
 dna_str = list(input())
 chk_acgt = list(map(int, input().split()))
 answer = 0
 
-# 슬라이딩 윈도우 활용 X
 for i in range(dna_len - pwd_len):
-    now_pwd = dna_str[i:i+pwd_len-1]
-    now_acgt = [now_pwd.count('A'), now_pwd.count('C'), now_pwd.count('G'), now_pwd.count('T')]
+    now_str = dna_str[i:i+pwd_len-1]
+    now_acgt = [now_str.count('A'), now_str.count('C'), now_str.count('G'), now_str.count('T')]
     for j in range(4):
         if now_acgt[j] < chk_acgt[j]:
             break
         answer += 1
+'''
 
 # 슬라이딩 윈도우 활용 O
+
+# 전역 변수 선언
+chk_acgt = [0] * 4 # 비밀번호 되기 위한 최소 a,c,g,t 개수
+now_acgt = [0] * 4 # 현재 문자열의 a,c,g,t 개수
+pass_cnt = 0       # 최소 조건 충족 여부 수
+
+# 함수 선언
+def add_char(c): # 슬라이딩 윈도우에 추가된 문자 처리
+    global chk_acgt, now_acgt, pass_cnt
+    if c == 'A':
+        now_acgt[0] += 1
+        if now_acgt[0] == chk_acgt[0]:
+            pass_cnt += 1
+    elif c == 'C':
+        now_acgt[1] += 1
+        if now_acgt[1] == chk_acgt[1]:
+            pass_cnt += 1
+    elif c == 'G':
+        now_acgt[2] += 1
+        if now_acgt[2] == chk_acgt[2]:
+            pass_cnt += 1
+    elif c == 'T':
+        now_acgt[2] += 1
+        if now_acgt[2] == chk_acgt[2]:
+            pass_cnt += 1
+
+def remove_char(c): # 슬라이딩 윈도우에서 제거된 문자 처리
+    global chk_acgt, now_acgt, pass_cnt
+    if c == 'A':
+        if now_acgt[0] == chk_acgt[0]:
+            pass_cnt -= 1
+        now_actg -= 1
+    elif c == 'C':
+        if now_acgt[1] == chk_acgt[1]:
+            pass_cnt -= 1
+        now_acgt -= 1        
+    elif c == 'G':
+        if now_acgt[2] == chk_acgt[2]:
+            pass_cnt -= 1
+        now_acgt -= 1
+    elif c == 'T':
+        if now_acgt[3] == chk_acgt[3]:
+            pass_cnt -= 1
+        now_acgt -= 1
+
+# 필요 변수 선언
+dna_len, pwd_len = map(int, input().split())
+dna_str = list(input())
+chk_acgt = list(map(int, input().split()))
+answer = 0
+
+# 초기 문자열 처리
 
 # 정답 출력
 print(answer)
